4615ff9182384e741c9148ef5d3edb1aeddfdf6d
Henry Tirla <Henrytirla@gmail.com>
2020-03-18 20:26:35 +0100

    Generate touches

---
+++ sre2020/CollectFiles.py
@@ -0,0 +1,94 @@
+import json
+from pip._vendor import requests
+import csv
+
+# @dictFiles empty dictionary of files
+# @lstTokens GitHub authentication tokens
+def countfiles(dictfiles, lsttokens, repo):
+    ipage = 1  # url page counter
+    ct = 0  # token counter
+
+    try:
+        # loop though all the commit pages until the last returned empty page
+        while True:
+            if ct == len(lstTokens):
+                ct = 0
+            spage = str(ipage)
+            commitsUrl = 'https://api.github.com/repos/' + repo + '/commits?page=' + spage + \
+                         '&per_page=100&access_token=' + lsttokens[ct]
+            ct += 1
+            content = requests.get(commitsUrl)
+            jsonCommits = json.loads(content.content)
+            # break out of the while loop if there are no more commits in the pages
+            if len(jsonCommits) == 0:
+                break
+            # iterate through the list of commits in a page
+            for shaObject in jsonCommits:
+                sha = shaObject['sha']
+                if ct == len(lstTokens):
+                    ct = 0
+                # For each commit, use the GitHub commit API to extract the files touched by the commit
+                shaUrl = 'https://api.github.com/repos/' + repo + '/commits/' + sha \
+                         + '?access_token=' + lstTokens[ct]
+                ct += 1
+                content = requests.get(shaUrl)
+                shaDetails = json.loads(content.content)
+                filesjson = shaDetails['files']
+                for filenameObj in filesjson:
+                    filename = filenameObj['filename']
+                    dictfiles[filename] = dictfiles.get(filename, 0) + 1
+            ipage += 1
+    except:
+        print("Error receiving data")
+        exit(0)
+
+repo = 'scottyab/rootbeer'
+# put your tokens here
+lstTokens = ['29f258c44f6bc4a4ffbc35e686f67f7aefe55395']
+
+# dictfiles = dict()
+# countfiles(dictfiles, lstTokens, repo)
+# print('Total number of files: ' + str(len(dictfiles)))
+#
+# file = repo.split('/')[1]
+# #change this to the path of your file
+# fileOutput = '/Users/macbookpro/Documents/MSC Software Engineering/YEAR ONE/SECOND SEMESTER/SOFTWARE REENGINEERING/Lab-session/Mining Repositories/file_'+file+'.csv'
+# rows = ["Filename", "Touches"]
+# fileCSV = open(fileOutput, 'w')
+# writer = csv.writer(fileCSV)
+# writer.writerow(rows)
+#
+# bigcount = None
+# bigfilename = None
+# for filename, count in dictfiles.items():
+#     rows = [filename, count]
+#     writer.writerow(rows)
+#     if bigcount is None or count > bigcount:
+#         bigcount = count
+#         bigfilename = filename
+# fileCSV.close()
+# print('The file ' + bigfilename + ' has been touched ' + str(bigcount) + ' times.')
+
+
+names = []
+dates = []
+dictfiles = dict()
+filenames = []
+countfiles(dictfiles, lstTokens, repo)
+print('Total number of files: ' + str(len(dictfiles)))
+
+
+file = repo.split('/')[1]
+#change this to the path of your file
+fileOutput = file+'.csv' #/Users/businge/Documents/00Mercy/sre2020/file_'+file+'.csv'
+rows = ["Filename", "Author", "Date"]
+fileCSV = open(fileOutput, 'w')
+writer = csv.writer(fileCSV)
+writer.writerow(rows)
+
+iterator = 0;
+for filename in filenames:
+    rows = [filename,  names[iterator], dates[iterator]]
+    iterator+=1
+    writer.writerow(rows)
+fileCSV.close()
---
+++ sre2020/henrytirla_scatterplot.py
@@ -0,0 +1,55 @@
+import csv
+from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+filename = "rootbeer.csv"
+
+files = []
+authors = []
+dates = []
+
+with open(filename, mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    count = 1
+    for rows in csv_reader:
+        if count > 100:
+            break
+        if count == 1 or count % 2 == 1:
+            count += 1
+            continue
+        count += 1
+        files.append(rows['Filename'])
+        authors.append(rows['Author'])
+        dates.append(datetime.strptime((rows['Date'].replace('T', ' ')).replace('Z', ''), '%Y-%m-%d %H:%M:%S'))
+
+dictFilesNames = {}
+dictFilesDates = {}
+dictNamesColors = {}
+filesWithNames = []
+filesWithDates = np.zeros(50)
+namesWithColors = []
+
+count = 1
+for file in files:
+    if file in dictFilesNames:
+        filesWithNames.append(dictFilesNames[file])
+    else:
+        dictFilesNames[file] = 'file ' + str(count)
+        count += 1
+        filesWithNames.append(dictFilesNames[file])
+
+for i in range(49, -1, -1):
+    if not files[i] in dictFilesDates:
+        dictFilesDates[files[i]] = dates[i]
+    filesWithDates[i] = int((dates[i] - dictFilesDates[files[i]]).days / 7)
+
+for author in authors:
+    if not author in dictNamesColors:
+        dictNamesColors[author] = "%06x" % random.randint(0, 0xFFFFFF)
+    namesWithColors.append('#' + dictNamesColors[author])
+
+plt.scatter(filesWithNames, filesWithDates, c=namesWithColors, s=50)
+plt.show()
