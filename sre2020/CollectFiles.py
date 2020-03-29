9f9d83fd0db0058cffbcef91d4826b1186ea9195
John Businge <johnxu21@gmail.com>
2020-03-29 19:48:33 +0200

    Update CollectFiles.py

--- sre2020/CollectFiles.py
+++ sre2020/CollectFiles.py
@@ -7,7 +7,6 @@ import csv
 def countfiles(dictfiles, lsttokens, repo):
     ipage = 1  # url page counter
     ct = 0  # token counter
-
     try:
         # loop though all the commit pages until the last returned empty page
         while True:
@@ -36,6 +35,7 @@ def countfiles(dictfiles, lsttokens, repo):
                 filesjson = shaDetails['files']
                 for filenameObj in filesjson:
                     filename = filenameObj['filename']
+                    print(filename)
                     dictfiles[filename] = dictfiles.get(filename, 0) + 1
             ipage += 1
     except:
@@ -44,51 +44,28 @@ def countfiles(dictfiles, lsttokens, repo):
 
 repo = 'scottyab/rootbeer'
 # put your tokens here
-lstTokens = ['29f258c44f6bc4a4ffbc35e686f67f7aefe55395']
-
-# dictfiles = dict()
-# countfiles(dictfiles, lstTokens, repo)
-# print('Total number of files: ' + str(len(dictfiles)))
-#
-# file = repo.split('/')[1]
-# #change this to the path of your file
-# fileOutput = '/Users/macbookpro/Documents/MSC Software Engineering/YEAR ONE/SECOND SEMESTER/SOFTWARE REENGINEERING/Lab-session/Mining Repositories/file_'+file+'.csv'
-# rows = ["Filename", "Touches"]
-# fileCSV = open(fileOutput, 'w')
-# writer = csv.writer(fileCSV)
-# writer.writerow(rows)
-#
-# bigcount = None
-# bigfilename = None
-# for filename, count in dictfiles.items():
-#     rows = [filename, count]
-#     writer.writerow(rows)
-#     if bigcount is None or count > bigcount:
-#         bigcount = count
-#         bigfilename = filename
-# fileCSV.close()
-# print('The file ' + bigfilename + ' has been touched ' + str(bigcount) + ' times.')
+lstTokens = ['']
 
 
-names = []
-dates = []
 dictfiles = dict()
-filenames = []
 countfiles(dictfiles, lstTokens, repo)
 print('Total number of files: ' + str(len(dictfiles)))
 
-
 file = repo.split('/')[1]
 #change this to the path of your file
-fileOutput = file+'.csv' #/Users/businge/Documents/00Mercy/sre2020/file_'+file+'.csv'
-rows = ["Filename", "Author", "Date"]
+fileOutput = file+'.csv'
+rows = ["Filename", "Touches"]
 fileCSV = open(fileOutput, 'w')
 writer = csv.writer(fileCSV)
 writer.writerow(rows)
 
-iterator = 0;
-for filename in filenames:
-    rows = [filename,  names[iterator], dates[iterator]]
-    iterator+=1
+bigcount = None
+bigfilename = None
+for filename, count in dictfiles.items():
+    rows = [filename, count]
     writer.writerow(rows)
+    if bigcount is None or count > bigcount:
+        bigcount = count
+        bigfilename = filename
 fileCSV.close()
+print('The file ' + bigfilename + ' has been touched ' + str(bigcount) + ' times.')
